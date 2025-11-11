275e959a7fe027d1b34fb8fee74b32cdc48b05af
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-11 16:46:10 +0200

    Add RespExcludeInterceptor to exclude sensitive user data from responses

---
+++ src/common/interceptor/resp-exclude/resp-exclude.interceptor.spec.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { RespExcludeInterceptor } from './resp-exclude.interceptor';
+
+describe('RespExcludeInterceptor', () => {
+  it('should be defined', () => {
+    expect(new RespExcludeInterceptor()).toBeDefined();
+  });
+});
---
+++ src/common/interceptor/resp-exclude/resp-exclude.interceptor.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable prettier/prettier */
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { userInfo } from 'os';
+import { map, Observable, tap } from 'rxjs';
+import { UserEntity } from 'src/moduls/user/entities/user.entity';
+
+@Injectable()
+export class RespExcludeInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log("RespExcludeInterceptor executed" , context.getHandler().name);
+    return next.handle().pipe(map((res) => {
+      if (Array.isArray(res.data)) {
+        res.data = res.data.map((item: UserEntity) => {
+          console.log("RespExcludeInterceptor item before exclude" , item);
+          const { password , role, ...rest } = item;
+          return rest;
+        });
+      } else {
+        console.log("RespExcludeInterceptor item before exclude" , res);
+        const { password, role , ...rest } = res.data;
+        return rest;
+      }
+      return res;
+    }));
+  }
+}
