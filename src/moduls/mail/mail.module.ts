59a3248509e95f19a2872c517055b6c3b597958f
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-11 16:49:37 +0200

    Implement mail module with service, templates, and tests for email functionality

---
+++ src/moduls/mail/mail.module.ts
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'node:path';
+import {EjsAdapter} from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+@Module({
+  providers: [MailService],
+  imports: [MailerModule.forRootAsync({
+    inject:[ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      transport: {
+        host: configService.get<string>('MAIL_HOST'),
+        port: configService.get<number>('MAIL_PORT'),
+        secure : process.env.NODE_ENV === 'production' ? true : false,
+        auth: {
+          user: configService.get<string>('MAIL_USERNAME'),
+          pass: configService.get<string>('MAIL_PASSWORD'),
+        },
+      },
+      template :{
+            dir:join(__dirname,'templates'),
+        adapter : new EjsAdapter({inlineCssEnabled:true}),
+      }
+    }),
+  })],
+  exports: [MailService],
+})
+export class MailModule {}
---
+++ src/moduls/mail/mail.service.ts
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MailService {
+
+    constructor(private readonly mailService: MailerService) { }
+    public async sendWelcomeEmail(email: string, name: string): Promise<void> {
+        // Implementation for sending welcome email
+        await this.mailService.sendMail({
+            to: email,
+            from: 'noreply@yourdomain.com',
+            subject: 'Welcome to MovieApp!',
+            template: 'emailtemplate',
+            context: { name: name, email: email }
+        });
+    }
+
+    public async sendVerificationEmail(email: string, name: string, token: string): Promise<void> {
+        const verificationLink = `${process.env.DOMAIN}/?token=${token}&email=${email}`;
+        await this.mailService.sendMail({
+            to: email,
+            from: 'noreply@yourdomain.com',
+            subject: 'Verify Your MovieApp Account',
+            template: 'verificationtemplate',
+            context: { name: name, verificationLink: verificationLink }
+        });
+    }
+}
