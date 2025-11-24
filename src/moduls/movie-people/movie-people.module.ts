1147f1285b0411b681495eb4ae96d60e3be2d112
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-24 15:51:09 +0200

    feat: update MoviePeopleModule to include additional imports and exports

--- src/moduls/movie-people/movie-people.module.ts
+++ src/moduls/movie-people/movie-people.module.ts
@@ -1,12 +1,20 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MoviePeopleController } from './movie-people.controller';
 import {MoviePeopleService} from "./movie-people.service"
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviePeopleEntity } from './entity/movie_people.entity';
+import { MovieModule } from '../movie/movie.module';
+import { PeopleModule } from '../people/people.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
 @Module({
   controllers: [MoviePeopleController],
   providers: [MoviePeopleService],
-  imports: [TypeOrmModule.forFeature([MoviePeopleEntity])],
+  imports: [TypeOrmModule.forFeature([MoviePeopleEntity]) , 
+    forwardRef(() => MovieModule)
+   , PeopleModule ,ConfigModule ,JwtModule],
+  exports: [MoviePeopleService],
 })
 export class MoviePeopleModule {}
