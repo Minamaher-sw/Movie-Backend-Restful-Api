fe30c8e0f0f89f393186f16438c4364e7f8b8986
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-24 15:49:58 +0200

    feat: add UUID validation for movie_id and person_id in CreateMoviePeopleDto

--- src/moduls/movie-people/dto/create-movie-people.dto.ts
+++ src/moduls/movie-people/dto/create-movie-people.dto.ts
@@ -1,9 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEnum } from "class-validator";
+import { IsEnum, IsUUID } from "class-validator";
 import { PeopleRole } from "../../../utils/enum";
 
 export class CreateMoviePeopleDto {
+
+    @ApiProperty({ example: "movie-uuid" })
+    @IsUUID()
+    movie_id: string;
+
+    @ApiProperty({ example: "person-uuid" })
+    @IsUUID()
+    person_id: string;
 
     @ApiProperty({ enum: PeopleRole, description: 'Role of the person in the movie' })
     @IsEnum(PeopleRole, { message: 'role must be a valid PeopleRole enum value' })
