acea92d6080b8e4e6d92416cd86296608bb5201c
Minamaher-sw <mosadefmena185@gmail.com>
2025-12-01 00:05:30 +0200

    return files before update

--- api/index.ts
+++ api/index.ts
@@ -3,17 +3,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable prettier/prettier */
-import { createApp } from '../src/main';
-import serverlessExpress from '@vendia/serverless-express';
+// import { createApp } from '../src/main';
+// import serverlessExpress from '@vendia/serverless-express';
 
-let server;
+// let server;
 
-export default async function handler(req, res) {
-  if (!server) {
-    const app = await createApp();
-    server = serverlessExpress({ app: app.getHttpAdapter().getInstance() });
-  }
-  return server(req, res);
-}
+// export default async function handler(req, res) {
+//   if (!server) {
+//     const app = await createApp();
+//     server = serverlessExpress({ app: app.getHttpAdapter().getInstance() });
+//   }
+//   return server(req, res);
+// }
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,78 +1,5 @@
 /* eslint-disable prettier/prettier */
 // /* eslint-disable prettier/prettier */
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { NestExpressApplication } from '@nestjs/platform-express';
-// import cookieParser from 'cookie-parser';
-// import { QueryExceptionFilter } from './common/filters/query-exception.filter';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-// import helmet from 'helmet';
-// import bodyParser from 'body-parser';
-
-// import {
-//   initializeTransactionalContext,
-//   addTransactionalDataSource,
-// } from 'typeorm-transactional';
-// import { DataSource } from 'typeorm';
-
-// async function bootstrap() {
-
-//   initializeTransactionalContext();
-
-//   // ❗ Important: Disable built-in bodyParser
-//   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-//     bodyParser: false,
-//   });
-
-//   const dataSource = app.get(DataSource);
-//   addTransactionalDataSource(dataSource);
-
-//   app.setGlobalPrefix('api/v1',{
-//      exclude: ['/']
-//   });
-
-//   // Stripe raw body ONLY for webhook
-//   app.use('/api/v1/payment/webhook',
-//     bodyParser.raw({ type: 'application/json' }),
-//   );
-
-//   // Normal body parsing for the rest of the app
-//   app.use(bodyParser.json({ limit: '10mb' }));
-//   app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-
-//   app.use(cookieParser());
-//   app.use(helmet());
-
-//   app.enableCors({
-//     origin: [
-//       'http://localhost:3000',
-//       'http://localhost:3001',
-//       'http://localhost:4200',
-//       'https://movieapp-frontend-nine.vercel.app',
-//     ],
-//     credentials: true,
-//   });
-
-//   app.useGlobalFilters(new QueryExceptionFilter());
-
-//   const swagger = new DocumentBuilder()
-//     .setTitle('Movie App API')
-//     .setDescription('The Movie App API description')
-//     .setVersion('1.0')
-//     .build();
-
-//   const document = SwaggerModule.createDocument(app, swagger);
-//   SwaggerModule.setup('api/v1/swagger', app, document);
-
-//   const port = process.env.PORT ?? 3001;
-
-//   await app.listen(port, () => {
-//     console.log(`🚀 Server is running on http://localhost:${port}/api/v1`);
-//   });
-// }
-
-// bootstrap();
-/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -88,10 +15,11 @@ import {
 } from 'typeorm-transactional';
 import { DataSource } from 'typeorm';
 
-export async function createApp() {
+async function bootstrap() {
 
   initializeTransactionalContext();
 
+  // ❗ Important: Disable built-in bodyParser
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bodyParser: false,
   });
@@ -99,14 +27,16 @@ export async function createApp() {
   const dataSource = app.get(DataSource);
   addTransactionalDataSource(dataSource);
 
-  app.setGlobalPrefix('api/v1', {
-    exclude: ['/'],
+  app.setGlobalPrefix('api/v1',{
+     exclude: ['/']
   });
 
+  // Stripe raw body ONLY for webhook
   app.use('/api/v1/payment/webhook',
     bodyParser.raw({ type: 'application/json' }),
   );
 
+  // Normal body parsing for the rest of the app
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
@@ -134,7 +64,77 @@ export async function createApp() {
   const document = SwaggerModule.createDocument(app, swagger);
   SwaggerModule.setup('api/v1/swagger', app, document);
 
-  await app.init();
+  const port = process.env.PORT ?? 3001;
 
-  return app;
+  await app.listen(port, () => {
+    console.log(`🚀 Server is running on http://localhost:${port}/api/v1`);
+  });
 }
+
+bootstrap();
+/* eslint-disable prettier/prettier */
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+// import cookieParser from 'cookie-parser';
+// import { QueryExceptionFilter } from './common/filters/query-exception.filter';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import helmet from 'helmet';
+// import bodyParser from 'body-parser';
+
+// import {
+//   initializeTransactionalContext,
+//   addTransactionalDataSource,
+// } from 'typeorm-transactional';
+// import { DataSource } from 'typeorm';
+
+// export async function createApp() {
+
+//   initializeTransactionalContext();
+
+//   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+//     bodyParser: false,
+//   });
+
+//   const dataSource = app.get(DataSource);
+//   addTransactionalDataSource(dataSource);
+
+//   app.setGlobalPrefix('api/v1', {
+//     exclude: ['/'],
+//   });
+
+//   app.use('/api/v1/payment/webhook',
+//     bodyParser.raw({ type: 'application/json' }),
+//   );
+
+//   app.use(bodyParser.json({ limit: '10mb' }));
+//   app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+//   app.use(cookieParser());
+//   app.use(helmet());
+
+//   app.enableCors({
+//     origin: [
+//       'http://localhost:3000',
+//       'http://localhost:3001',
+//       'http://localhost:4200',
+//       'https://movieapp-frontend-nine.vercel.app',
+//     ],
+//     credentials: true,
+//   });
+
+//   app.useGlobalFilters(new QueryExceptionFilter());
+
+//   const swagger = new DocumentBuilder()
+//     .setTitle('Movie App API')
+//     .setDescription('The Movie App API description')
+//     .setVersion('1.0')
+//     .build();
+
+//   const document = SwaggerModule.createDocument(app, swagger);
+//   SwaggerModule.setup('api/v1/swagger', app, document);
+
+//   await app.init();
+
+//   return app;
+// }
